a6c910e2fdf810bde72530964417f122152729ff
ShravanTheCoder <92569405+ShravanTheCoder@users.noreply.github.com>
2022-03-30 16:26:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas = createCanvas(500,500);
+    canvas.position(700,350);
+    background(255,255,255);
+}
+
+function draw() {
+    
+}
